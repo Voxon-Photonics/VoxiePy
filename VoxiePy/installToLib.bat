b79254e7dbe7fd25ec4eaf480081e6831df7b5c0
Matthew Vecchio <Matthew.Vecchio@gmail.com>
2023-07-26 14:22:29 +0930

    Update installToLib.bat

--- VoxiePy/installToLib.bat
+++ VoxiePy/installToLib.bat
@@ -2,18 +2,18 @@ cls
 set thisPath=%cd%
 
 @echo off
-echo. VxPy -> A Python wrapper for Voxon Development
+echo. VoxiePy -> A Python wrapper for Voxon Development
 echo. 2023 - Matthew Vecchio for Voxon. 
 echo.
-echo. This batch script will install VxPy to your Python 3 library
-echo. VxPy has been developed to work with Python 3.11 but its likely 
+echo. This batch script will install VoxiePy to your Python 3 library
+echo. VoxiePy has been developed to work with Python 3.11 but its likely 
 echo. to work on older versions
 echo.
 echo. * Please ensure you have Pyhton 3 installed prior to running this script
 echo.
 echo.
-echo. Press 1 to Add / Install VxPy to your Python 3's library folder 
-echo. Press 2 to Remove / Uninstall VxPy from your Python 3's library folder
+echo. Press 1 to Add / Install VoxiePy to your Python 3's library folder 
+echo. Press 2 to Remove / Uninstall VoxiePy from your Python 3's library folder
 echo. Press 3 or Q to Quit This batch script.
 choice /c 123Q 
 if errorlevel 4 Goto Exit
@@ -22,26 +22,26 @@ if errorlevel 2 Goto Uninstall
 if errorlevel 1 Goto Install
 
 :Uninstall
-echo. VxPy Removing
+echo. VoxPy Removing
 
 C:
 cd %LocalAppData%\Programs\Python\Python3*\Lib
 set installPath=%cd%
-dir "vx*.py"
+dir "vx*.py" "vox*.py"
 
 Del %installPath%\VxScanCodes.py
-Del %installPath%\VxPy.py 
+Del %installPath%\VoxiePy.py 
 Del %installPath%\VxPyDataTypes.py
 Del %installPath%\VxPyTools.py
 cd installPath
-dir "vx*.py"
+dir "vx*.py" "vox*.py"
 
-echo. VxPy Removed from  Python 3's Lib folder
+echo. VoxiePy Removed from  Python 3's Lib folder
 pause
 goto Exit
 
 :Install 
-echo. VxPy Installing
+echo. VoxiePy Installing
 
 C:
 cd %LocalAppData%\Programs\Python\Python3*\Lib
@@ -50,7 +50,7 @@ set installPath=%cd%
 Copy "%thisPath%\VxScanCodes.py" %installPath%
 Copy "%thisPath%\VxPyDataTypes.py" %installPath%
 Copy "%thisPath%\VxPyTools.py" %installPath%
-Copy "%thisPath%\VxPy.py" %installPath%
+Copy "%thisPath%\VoxiePy.py" %installPath%
 
 
 cd installPath
